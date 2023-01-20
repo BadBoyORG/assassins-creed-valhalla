6c75ffffed9c1b18fedc84430a46b2148da52f94
Brian <sbriansantos2002@gmail.com>
2023-01-20 19:31:12 -0300

    section story init

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ document.querySelector("#app").innerHTML = `
         <img 
           src="./src/assets/home-picture.png" alt="Illustrative image of the game" 
         />
-        <h1><span>P</span>lay a viking name eivor who as been trained since childhood to become warrior</h1> 
+        <p><span>P</span>lay a viking name eivor who as been trained since childhood to become warrior</p> 
         <a href="https://store.ubi.com/ofertas/assassins-creed-valhalla/5e8353ff5cdf9a21c0b4e72e.html?lang=en " target="_blank">buy now</a>
         <div class="cards">   
           <img
@@ -39,8 +39,30 @@ document.querySelector("#app").innerHTML = `
         </div>
       </div>
     </section>
-    <section>
-      <h1>Valhalla</h1>
+    <section id="story">
+      <div class="container">
+        <h1 class="title">story</h1>
+        <div class="text-grid">
+          <p class="text-left">
+            <span>S</span>et in <span>873 CE,</span> <span>V</span>alhalla has players take the role of <span>E</span>ivor<span>,</span> a <span>V</span>iking <span>R</span>aider and <span>C</span>lan <span>L</span>eader of the <span>N</span>orsemen who can be played either as male or female<span>. T</span>he game retains the dialogue choice and <span>NPC</span> relationship elements from <span>A</span>ssassin's <span>C</span>reed
+          </p>
+          <p class="text-right">
+            <span>O</span>dyssey<span>,</span> with new elements such as forging political alliances with <span>NPC</span> clans<span>. E</span>very choice and decision of the players affects the world of the game in real time<span>. P</span>layers can travel from the cold shores of <span>N</span>orway to <span>E</span>ngland<span>...</span> 
+          </p>
+        </div>
+        <figure>
+          <img 
+            src="./src/assets/story-picture.jpg"
+            alt="Illustrative image of the game"
+          />
+          <figcaption>
+            <span>A D</span>ark <span>A</span>ge <span>O</span>pen <span>W</span>orld
+          </figcaption>
+        </figure>
+        <div class="social-networks">
+        
+        </div>
+      </div>
     </section>
   </main>
 `
